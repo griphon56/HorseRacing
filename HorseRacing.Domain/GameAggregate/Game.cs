6d6ce19f8a043faa29cfa6787ccc6688278931a4
Vladimir Solianikov <Griphon56@gmail.com>
2025-03-03 22:14:48 +0300

    Конфигурация сущностей игры

--- HorseRacing.Domain/GameAggregate/Game.cs
+++ HorseRacing.Domain/GameAggregate/Game.cs
@@ -1,4 +1,5 @@
 ﻿using HorseRacing.Domain.Common.Models.Base;
+using HorseRacing.Domain.GameAggregate.Entities;
 using HorseRacing.Domain.GameAggregate.Enums;
 using HorseRacing.Domain.GameAggregate.ValueObjects;
 using System.ComponentModel.DataAnnotations;
@@ -28,21 +29,57 @@ namespace HorseRacing.Domain.GameAggregate
         /// </summary>
         public DateTime  DateEnd { get; private set; }
 
+        /// <summary>
+        /// Результат игры
+        /// </summary>
+        private readonly GameResult _gameResult;
+        public GameResult GameResult => _gameResult;
+
+        /// <summary>
+		/// Колода карт
+		/// </summary>
+		private readonly List<GameDeckCard> _gameDeckCards = new();
+        public IReadOnlyList<GameDeckCard> GameDeckCards => _gameDeckCards.AsReadOnly();
+
+        /// <summary>
+		/// События игры
+		/// </summary>
+		private readonly List<GameEvent> _gameEvents = new();
+        public IReadOnlyList<GameEvent> GameEvents => _gameEvents.AsReadOnly();
+
+        /// <summary>
+		/// Позиция лошадей
+		/// </summary>
+		private readonly List<GameHorsePosition> _gameHorsePositions = new();
+        public IReadOnlyList<GameHorsePosition> GameHorsePositions => _gameHorsePositions.AsReadOnly();
+
+        /// <summary>
+		/// Игроки
+		/// </summary>
+		private readonly List<GamePlayer> _gamePlayers = new();
+        public IReadOnlyList<GamePlayer> GamePlayers => _gamePlayers.AsReadOnly();
+
         private Game () : base(GameId.CreateUnique(), new EntityChangeInfo(DateTime.UtcNow)) { }
 
-        private Game(GameId id, StatusType status, string name, DateTime dateStart, DateTime dateEnd, EntityChangeInfo changeInfo)
-             : base(id ?? GameId.CreateUnique(), changeInfo)
+        private Game(GameId id, string name, StatusType status, DateTime dateStart, DateTime dateEnd, List<GameDeckCard> gameDeckCards
+            , List<GameHorsePosition> gameHorsePositions, List<GamePlayer> gamePlayers, EntityChangeInfo changeInfo, List<GameEvent>? gameEvents=null, GameResult? gameResult=null)
+            : base(id ?? GameId.CreateUnique(), changeInfo)
         {
-            Status = status;
             Name = name;
+            Status = status;
             DateStart = dateStart;
             DateEnd = dateEnd;
+            _gameDeckCards = gameDeckCards;
+            _gameHorsePositions = gameHorsePositions;
+            _gamePlayers = gamePlayers;
+            _gameEvents = gameEvents ?? new();
+            _gameResult = gameResult;
         }
 
-        public static Game Create(GameId id, StatusType
-            status, string name, DateTime dateStart, DateTime dateEnd, EntityChangeInfo changeInfo)
+        public static Game Create(GameId id, string name, StatusType status, DateTime dateStart, DateTime dateEnd, List<GameDeckCard> gameDeckCards
+            , List<GameHorsePosition> gameHorsePositions, List<GamePlayer> gamePlayers, EntityChangeInfo changeInfo, List<GameEvent>? gameEvents = null, GameResult? gameResult = null)
         {
-            return new Game(id, status, name, dateStart, dateEnd, changeInfo);
+            return new Game(id, name, status, dateStart, dateEnd, gameDeckCards, gameHorsePositions, gamePlayers, changeInfo, gameEvents, gameResult);
         }
     }
 }
---
+++ HorseRacing.Infrastructure/Persistence/Configurations/GameConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using HorseRacing.Domain.GameAggregate;
+using HorseRacing.Domain.GameAggregate.Entities;
+using HorseRacing.Domain.GameAggregate.ValueObjects;
+using HorseRacing.Domain.UserAggregate;
+using HorseRacing.Domain.UserAggregate.ValueObjects;
+using HorseRacing.Infrastructure.Persistence.Configurations.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HorseRacing.Infrastructure.Persistence.Configurations
+{
+    public class GameConfiguration : IEntityTypeConfiguration<Game>
+    {
+        /// <summary>
+        /// Настройка таблицы пользователей
+        /// </summary>
+        /// <param name="builder">Строитель.</param>
+        public void Configure(EntityTypeBuilder<Game> builder)
+        {
+            ConfigureGameTable(builder);
+            ConfigureGameDeckCardTable(builder);
+            ConfigureGameResultTable(builder);
+        }
+
+        /// <summary>
+        /// Configures the user table.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        private void ConfigureGameTable(EntityTypeBuilder<Game> builder)
+        {
+            builder.ToTable("Games");
+
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Id).ValueGeneratedNever().HasConversion(id => id.Value,
+                value => GameId.Create(value));
+
+            builder.Property(m => m.Name).HasMaxLength(100);
+
+            BaseChangeInfoConfigurationUtilities.AttachChangeInfoForConfiguration<Game, GameId>(builder);
+        }
+
+        private void ConfigureGameDeckCardTable(EntityTypeBuilder<Game> builder)
+        {
+            builder.OwnsMany(u => u.GameDeckCards, a =>
+            {
+                a.ToTable("GameDeckCards");
+                a.WithOwner().HasForeignKey(m => m.GameId);
+                a.HasKey(m => m.Id);
+                a.Property(m => m.Id).ValueGeneratedNever().HasConversion(id => id.Value, value => GameDeckCardId.Create(value));
+                a.Property(m => m.GameId).HasConversion(id => id.Value, value => GameId.Create(value));
+            });
+
+            builder.Metadata.FindNavigation(nameof(User.Account))!.SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+
+        private void ConfigureGameResultTable(EntityTypeBuilder<Game> builder)
+        {
+            builder.OwnsOne(u => u.GameResult, a =>
+            {
+                a.ToTable("GameResults");
+                a.WithOwner().HasForeignKey(m => m.GameId);
+                a.HasKey(m => m.Id);
+                a.Property(m => m.Id).ValueGeneratedNever().HasConversion(id => id.Value, value => GameResultId.Create(value));
+                a.Property(m => m.GameId).HasConversion(id => id.Value, value => GameId.Create(value));
+            });
+
+            builder.Metadata.FindNavigation(nameof(User.Account))!.SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+    }
+}
